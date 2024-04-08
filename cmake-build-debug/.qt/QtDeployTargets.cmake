f57773dd2d698398ff20460ccb300318d0274010
Ivan Ivanov <me@ivanov.pro>
2024-04-08 14:41:46 +0300

    clear track files

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_tcp_broker_FILE /home/ivanovx/Desktop/tcp-broker/cmake-build-debug/tcp_broker)
--- cmake-build-release-system/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_tcp_broker_FILE /home/ivanovx/Desktop/tcp-brocker/cmake-build-release-system/tcp_broker)
